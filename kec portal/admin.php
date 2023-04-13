60c28c15e468a4289f26b27b987fa887095d74c0
E.vajjiravel <106234463+vajjiravel@users.noreply.github.com>
2023-04-13 11:48:19 +0530

    Add files via upload

---
+++ kec portal/admin.php
@@ -0,0 +1,45 @@
+<?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+
+    $con = new mysqli("localhost","root","","project");
+    if($con->connect_error) {
+        die("Failed to connect : ".$con->connect_error);
+    } else {
+        $stmt = $con->prepare("select * from admin where username = ?");
+        $stmt->bind_param("s",$username);
+        $stmt->execute();
+        $stmt_result = $stmt->get_result();
+        if($stmt_result->num_rows > 0){
+            $data = $stmt_result->fetch_assoc();
+            if($data['password'] === $password){
+                $_SESSION['password'] = $password_login;
+                header('location: ADMIN.HTML');
+            }else {
+                $_SESSION['password'] = $password_login;
+                header('location: index.html');
+            }
+        } else {
+            $_SESSION['password'] = $password_login;
+            header('location: index.html');
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
